e3db172d8dbaa278d51e2a856398a2c6b1ac4090
Giralis <vladimirmail895@gmail.com>
2021-05-28 23:01:53 +0300

    Education project using APOD

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var copyrightLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    let photoInfoController = PhotoInfoController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                self.updateUI(with: photoInfo)
+            }
+        }
     }
-
+    
+    func updateUI(with photoInfo: PhotoInfo) {
+        guard let url = photoInfo.url.withHTTPS() else { return }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            guard let data = data,
+                  let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.title = photoInfo.title
+                self.imageView.image = image
+                self.descriptionLabel.text = photoInfo.description
+                
+                if let copyright = photoInfo.copyright {
+                    self.copyrightLabel.text = "Copyright \(copyright)"
+                } else {
+                    self.copyrightLabel.isHidden = true
+                }
+            }
+        })
+        task.resume()
+        }
 
 }
 
